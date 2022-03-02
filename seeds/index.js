34767cc011a2eeba5a197c22ad88765d3787b653
JoitaEduard <74983329+JoitaEduard@users.noreply.github.com>
2022-03-02 11:33:37 +0200

    Adaugare Erori + Frontend: EJS

--- seeds/index.js
+++ seeds/index.js
@@ -19,9 +19,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random].city}, ${cities[random].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo modi assumenda fugit? Numquam, sit suscipit nihil natus voluptatem quo voluptates inventore provident at, commodi dolore consectetur tempore eaque, neque consequuntur.',
+            price
         })
         await camp.save();
     }
