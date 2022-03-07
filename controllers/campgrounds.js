51aff222568a1fba0e8dc9d62a896490a859a652
JoitaEduard <74983329+JoitaEduard@users.noreply.github.com>
2022-03-07 20:48:06 +0200

    Controllers, Routes.route si Rating facut prin stele

---
+++ controllers/campgrounds.js
@@ -0,0 +1,64 @@
+const Campground = require('../models/campground');
+
+module.exports.index = async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
+}
+
+module.exports.renderNewForm = (req, res) => {
+    res.render('campgrounds/new');
+}
+
+module.exports.createCampground = async (req, res) => {
+    // if (!req.body.campground) {
+    //     throw new ExpressError('Invalid campground data', 400);
+    // }
+    const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
+    await campground.save();
+    req.flash('success', 'Succesfully made a new campground');
+    res.redirect(`/campgrounds/${campground._id}`);
+}
+
+module.exports.showCampground = async (req, res) => {
+    const campground = await Campground.findById(req.params.id).populate({
+        path: 'reviews',
+        populate: {
+            path: 'author'
+        }
+    }).populate('author');
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds');
+    }
+    res.render('campgrounds/show', { campground, msg: req.flash.success });
+}
+
+module.exports.renderEditForm = async (req, res) => {
+    const id = req.params.id;
+    const campground = await Campground.findById(id);
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground!');
+        return res.redirect('/campgrounds');
+    }
+    res.render('campgrounds/edit', { campground });
+}
+
+module.exports.updateCampground = async (req, res) => {
+    const id = req.params.id;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    req.flash('success', 'Successfully updated campground');
+    res.redirect(`/campgrounds/${id}`);
+}
+
+module.exports.deleteCampground = async (req, res) => {
+    const id = req.params.id;
+    const newCamp = await Campground.findById(id);
+    if (!newCamp.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    await Campground.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted campground');
+    res.redirect('/campgrounds');
+}
