86b00adf531de3fc77bb6fe839fbd3719e754cad
JoitaEduard <74983329+JoitaEduard@users.noreply.github.com>
2022-03-14 14:51:15 +0200

    add start script

--- app.js
+++ app.js
@@ -159,6 +159,8 @@ app.use((err, req, res, next) => {
     // res.send('Oh boy, something went wrong');
 })
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}.`);
 })
